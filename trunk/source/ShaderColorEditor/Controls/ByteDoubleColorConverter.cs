0c407a71cd7ed9feeb9c38896fbda68aa026b7eb
nAXAH1@gmail.com <nAXAH1@gmail.com@c3a95e5d-f258-4ed5-c9b2-91fe54d63e72>
2014-10-17 14:15:27 +0000

    Text input works

---
+++ trunk/source/ShaderColorEditor/Controls/ByteDoubleColorConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Kefir.ShaderColorEditor.Model;
+
+namespace Kefir.ShaderColorEditor.Controls
+{
+    internal class ByteDoubleColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(value);
+        }
+
+        private static object Convert(object value)
+        {
+            if (value is double)
+            {
+                return ColorExtensions.DoubleToByteComponent((double) value);
+            }
+            if (value is byte)
+            {
+                return ColorExtensions.ByteToDoubleComponent((byte) value);
+            }
+            var s = value as string;
+            if (s != null)
+            {
+                byte b;
+                if (byte.TryParse(s, out b))
+                {
+                    return ColorExtensions.ByteToDoubleComponent(b);
+                }
+            }
+
+            return value;
+        }
+    }
+}
